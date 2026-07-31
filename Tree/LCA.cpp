13f1b832970b184509f48ed0b988891f4ce4d3f1
Rishit Bhardwaj <bhardwajrishit2@gmail.com>
2026-07-31 09:39:27 +0530

    lca

---
+++ Tree/LCA.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL || root == p || root == q)
+            return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == NULL)
+            return right;
+        else if (right == NULL)
+            return left;
+        else
+            return root;
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(1);
+
+    root->left->left = new TreeNode(6);
+    root->left->right = new TreeNode(2);
+
+    root->right->left = new TreeNode(0);
+    root->right->right = new TreeNode(8);
+
+    root->left->right->left = new TreeNode(7);
+    root->left->right->right = new TreeNode(4);
+
+    Solution obj;
+
+    TreeNode* p = root->left;
+    TreeNode* q = root->left->right->right;
+
+    TreeNode* ans = obj.lowestCommonAncestor(root, p, q);
+
+    if (ans != NULL)
+        cout << ans->val;
+
+    return 0;
+}
