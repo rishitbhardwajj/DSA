277127f1f7584a9d42c6e7b2feccc4561ed41f98
Rishit Bhardwaj <bhardwajrishit2@gmail.com>
2026-08-16 09:48:18 +0530

    no

---
+++ Graph/FloodFill.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    void dfs(int row, int col, vector<vector<int>>& ans,
+             vector<vector<int>>& image, int newColor,
+             int delRow[], int delCol[], int iniColor) {
+
+        ans[row][col] = newColor;
+
+        int n = image.size();
+        int m = image[0].size();
+
+        for(int i = 0; i < 4; i++) {
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+
+            if(nrow >= 0 && nrow < n &&
+               ncol >= 0 && ncol < m &&
+               image[nrow][ncol] == iniColor &&
+               ans[nrow][ncol] != newColor) {
+
+                dfs(nrow, ncol, ans, image, newColor,
+                    delRow, delCol, iniColor);
+            }
+        }
+    }
+
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image,
+                                   int sr, int sc, int newColor) {
+
+        int iniColor = image[sr][sc];
+
+        vector<vector<int>> ans = image;
+
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, +1, 0, -1};
+
+        dfs(sr, sc, ans, image, newColor,
+            delRow, delCol, iniColor);
+
+        return ans;
+    }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> image(n, vector<int>(m));
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> image[i][j];
+        }
+    }
+
+    int sr, sc, newColor;
+    cin >> sr >> sc >> newColor;
+
+    Solution obj;
+
+    vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
