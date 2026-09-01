a9a8852c64dabc03d795d871db369820025905ac
Rishit Bhardwaj <bhardwajrishit2@gmail.com>
2026-09-01 23:54:15 +0530

    sort

---
+++ C++ STL/sort.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {40, 10, 30, 20};
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
